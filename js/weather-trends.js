337d52b2c717f2b0de63e46f4c7c6338cd68ad0d
Jason Lotito <jasonlotito@gmail.com>
2014-04-11 22:25:52 -0400

    Getting API

---
+++ js/weather-trends.js
@@ -0,0 +1,21 @@
+
+function WeatherTrends ()
+{
+
+}
+
+WeatherTrends.prototype.getCurrentWeather = function(cb)
+{
+  $.get('http://www.weathertrends360.com/data/forecast/hourly-1day?key=g2frbigj3c8o4nl08mvizd5d9nrj0dzqfehqwyazahne60odvj&l=Bethlehem,PA&fmt=json', function(data){
+    cb(data);
+  });
+}
+$hour = $('#hour');
+var w = new WeatherTrends();
+w.getCurrentWeather(function(weather){
+//  document.write(JSON.stringify(weather));
+  for(var x in weather.forecast) {
+    var hour = weather.forecast[x];
+    $hour.append('<img src="http://www.weathertrends360.com/images/wxIcons/' + hour.iconLg + '">')
+  }
+})
