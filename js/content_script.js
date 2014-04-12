b5da982b0c7863d7cf6a8b52aa7fa980c0f9cf63
Jason Lotito <jasonlotito@gmail.com>
2014-04-12 12:22:06 -0400

    updating

--- js/content_script.js
+++ js/content_script.js
@@ -1,1 +1,38 @@
-console.log('Hello world');
+
+$((function(){
+  var w = new WeatherTrends();
+  w.getCurrentWeather(function(data){
+    var currentWeather = data.forecast[0];
+    var currentWeatherIcon = currentWeather.iconLg;
+    this.ePopup = document.createElement('div');
+    this.ePopup.id = "weather-hack";
+    var img = document.createElement('img');
+
+    img.src = chrome.extension.getURL('img/weather/colored/' + currentWeatherIcon);
+    img.style.height = '160px';
+    img.style.width = '160px';
+    img.style.margin = '10px';
+
+
+    var text = WeatherTrends.formatTemp(currentWeather.avgTempF) + '˚ and ' + currentWeather.wx;
+
+
+    var desc = document.createElement('p');
+    var closeIcon = document.createElement('img');
+    closeIcon.className = 'weatherhack-weather-icon-close';
+    var url = chrome.extension.getURL('img/close.png');
+    closeIcon.src = url;
+    closeIcon.addEventListener('click', function(){
+      this.ePopup.remove();
+    }.bind(this));
+    var text = document.createTextNode(text)
+    desc.appendChild(text);
+    desc.className = 'weatherhack-desc';
+
+    this.ePopup.appendChild(closeIcon);
+    this.ePopup.appendChild(img);
+    this.ePopup.appendChild(desc);
+
+    document.body.appendChild(this.ePopup);
+  });
+}));
--- js/weather-trends.js
+++ js/weather-trends.js
@@ -9,7 +9,13 @@ WeatherTrends.prototype.getCurrentWeather = function(cb)
   $.get('http://www.weathertrends360.com/data/forecast/hourly-1day?key=g2frbigj3c8o4nl08mvizd5d9nrj0dzqfehqwyazahne60odvj&l=Bethlehem,PA&fmt=json', function(data){
     cb(data);
   });
+};
+
+WeatherTrends.formatTemp = function( temp )
+{
+  return parseInt(temp, 10);
 }
+
 $hour = $('#hour');
 var w = new WeatherTrends();
 w.getCurrentWeather(function(weather){
